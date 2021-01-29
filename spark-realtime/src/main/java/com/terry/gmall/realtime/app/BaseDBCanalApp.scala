44f71c77cfdc5285dcb0e8a1dfaa79956dd7bb97
terry <921588402@qq.com>
2021-01-29 20:40:07 +0800

    将canal采集到的业务数据写入kafka

---
+++ spark-realtime/src/main/java/com/terry/gmall/realtime/app/BaseDBCanalApp.scala
@@ -0,0 +1,116 @@
+package com.terry.gmall.realtime.app
+
+import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
+import com.terry.gmall.realtime.utils.{MykafkaSink, MykafkaUtil, OffsetManagerUtil}
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.spark.SparkConf
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.kafka010.{HasOffsetRanges, OffsetRange}
+
+object BaseDBCanalApp {
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[4]").setAppName("base_db_canal_app")
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+    val topic = "ODS_BASE_DB_C"
+    val groupid = "base_db_canal_app_group"
+
+    //读取偏移量 的初始值
+    val offsetMap: Map[TopicPartition, Long] = OffsetManagerUtil.getOffset(topic,groupid)
+    var inputDStream:InputDStream[ConsumerRecord[String, String]] = null
+    //如果offsetMap有值，则从指定位置获取数据流，否则从最新位置获取数据流
+    if (offsetMap == null){
+      inputDStream =  MykafkaUtil.getKafkaStream(topic,ssc,groupid)
+    } else {
+      inputDStream = MykafkaUtil.getKafkaStream(topic,ssc,offsetMap,groupid)
+    }
+    //获取偏移量结束点 OffsetRange[]
+    var offsetRanges:Array[OffsetRange] = null
+    //从数据流中顺带把本批次的偏移量结束点存入全局变量中
+    val inputDstreamWithOffsetDstream: DStream[ConsumerRecord[String, String]] = inputDStream.transform { rdd =>
+      val hasOffsetRanges: HasOffsetRanges = rdd.asInstanceOf[HasOffsetRanges]
+      offsetRanges = hasOffsetRanges.offsetRanges
+      rdd
+    }
+
+    //把格式转化为jsonObject便于中间的业务处理
+    val jsonObjDstream: DStream[JSONObject] = inputDstreamWithOffsetDstream.map(record=>JSON.parseObject(record.value()))
+
+    jsonObjDstream.foreachRDD{rdd=>
+
+      rdd.foreachPartition{jsonItr=>
+        val dimTable = Array("user_info","base_province")
+        val factTable = Array("order_info","order_detail")
+
+        for (jsonObj <- jsonItr ) {
+          val table: String = jsonObj.getString("table")
+          val optType: String = jsonObj.getString("type")
+          val pkNames: JSONArray = jsonObj.getJSONArray("pkNames")
+          val dataArr: JSONArray = jsonObj.getJSONArray("data")
+
+          /*
+          将维表写入Hbase
+          命令行：put '命名空间:表名','RowKey','列族:列名','列值'
+          namespace:GMALL
+          teble:DIM+teble
+          faimly:INFO
+          rowkey：大表（user）需要预分区，需要将rowkey打散，小表（province）不需要
+          rowkey:将数据的主键先补全0，填充位数预计是数据上限，然后再反转
+           */
+          if(dimTable.contains(table)){
+            //获得主键
+            val pkName: String = pkNames.getString(0)
+
+            import collection.JavaConverters._
+            for ( data<- dataArr.asScala ) {
+              val dataJsonObj: JSONObject = data.asInstanceOf[JSONObject]
+              val pk: String = dataJsonObj.getString(pkName)
+
+              //将数据主键1先补上0，位数预计数数据的上限，再反转
+              val rowkey: String = StringUtils.leftPad(pk,10,"0").reverse
+              val namespace:String = "GMALL"
+              val hbaseTable:String = namespace+":"+"DIM_"+table.toUpperCase
+              val family = "INFO"
+              val column_value = data
+
+            }
+          }
+
+          //将事实表写入kafka
+          if (factTable.contains(table)){
+            //确定操作类型
+            var opt:String=null
+            if (optType.equals("INSERT")){
+              opt = "I"
+            }else if(optType.equals("UPDATE")){
+              opt = "U"
+            }else if(optType.equals("DELETE")){
+              opt = "D"
+            }
+
+            //topic:ODS+table+操作类型 message：data
+            val topic = "DWD_"+table.toUpperCase()+"_"+opt
+            //有可能一条canal的数据有多行操作，要将dataArr遍历
+            import collection.JavaConverters._
+            for ( data<- dataArr.asScala ) {
+              val dataJsonObj: JSONObject = data.asInstanceOf[JSONObject]
+              MykafkaSink.send(topic,dataJsonObj.toJSONString)
+            }
+
+          }
+          //println(jsonObj)
+        }
+
+      }
+      OffsetManagerUtil.saveOffset(topic,groupid,offsetRanges)
+
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
---
+++ spark-realtime/src/main/java/com/terry/gmall/realtime/utils/MykafkaSink.scala
@@ -0,0 +1,42 @@
+package com.terry.gmall.realtime.utils
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+object MykafkaSink {
+  private val properties:Properties = PropertiesUtil.load("config.properties")
+  val broker_list: String = properties.getProperty("kafka.broker.list")
+  var kafkaProducer:KafkaProducer[String,String] = null
+
+  def createKafkaProducer: KafkaProducer[String, String] = {
+    val properties = new Properties
+    properties.put("bootstrap.servers", broker_list)
+    properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    properties.put("enable.idompotence",(true: java.lang.Boolean))
+    var producer: KafkaProducer[String, String] = null
+    try
+      producer = new KafkaProducer[String, String](properties)
+    catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    producer
+  }
+
+  // 轮询分区 2.x  黏性分区3.x
+  def send(topic: String, msg: String): Unit = {
+    if (kafkaProducer == null) kafkaProducer = createKafkaProducer
+    kafkaProducer.send(new ProducerRecord[String, String](topic, msg))
+
+  }
+
+  // 根据 分区键分区
+  def send(topic: String,key:String, msg: String): Unit = {
+    if (kafkaProducer == null) kafkaProducer = createKafkaProducer
+    kafkaProducer.send(new ProducerRecord[String, String](topic,key, msg))
+
+  }
+
+}
